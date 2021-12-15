b68bf3f9cb66b90d888e68a5e7d22999f1bdf65d
shbodake <sayalibodake28@gmail.com>
2021-12-16 00:30:17 +0530

    Added: HelloWorld spring app to display hello message in web page

---
+++ src/main/java/com/example/demo/controller/HelloWebController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWebController {
+	@GetMapping ("/Web")
+	public String hello() {
+		return "hello";
+	}
+	
+	@GetMapping("/web/message")
+	public String message(Model model) {
+		model.addAttribute("message","This is a message");
+		return "message";
+	}
+}
