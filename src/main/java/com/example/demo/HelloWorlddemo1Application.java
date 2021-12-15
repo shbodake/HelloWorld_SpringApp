0f147836fab21d868d322c896b7da58d28f3b3f3
shbodake <sayalibodake28@gmail.com>
2021-12-15 23:46:42 +0530

    Added: To display hello World message

---
+++ src/main/java/com/example/demo/HelloWorlddemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorlddemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorlddemo1Application.class, args);
+	}
+
+}
