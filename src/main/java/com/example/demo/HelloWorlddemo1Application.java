59fe7552ca00473a698132c26a2dc5b78fc346fc
shbodake <sayalibodake28@gmail.com>
2021-12-15 23:53:08 +0530

    Added: HelloWorld spring app to display hello message

--- src/main/java/com/example/demo/HelloWorlddemo1Application.java
+++ src/main/java/com/example/demo/HelloWorlddemo1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorlddemo1Application {
 
 	public static void main(String[] args) {
+		System.out.println("Hello from Bridgelabz");
 		SpringApplication.run(HelloWorlddemo1Application.class, args);
 	}
 
